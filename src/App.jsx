bfce2ef1258920f22e7bf06a9d8af22563a29f27
shasherazi <hassanrandomz@gmail.com>
2023-07-22 17:56:42 +0500

    Add splash page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Splash from "./pages/Splash";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
