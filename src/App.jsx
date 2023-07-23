92cc70aef2e6bc4718de1ad47b2f5ebdefcdd05b
Imran Hossain <blur-imran@hotmail.com>
2023-07-23 06:02:30 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <Home />;
+  return <HomePage />;
 }
 
 export default App;
