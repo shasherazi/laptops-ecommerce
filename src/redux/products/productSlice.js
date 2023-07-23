75e687bea76124f4ac9f3d775895fddddd109b5e
Imran Hossain <blur-imran@hotmail.com>
2023-07-23 06:03:27 +0600

    Create productSlice.js

---
+++ src/redux/products/productSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const productSlice = createSlice({
+  name: 'products',
+  initialState: {
+    products: [], 
+  },
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload; 
+    },
+  },
+});
+
+export const { setProducts } = productSlice.actions;
+export default productSlice.reducer;
