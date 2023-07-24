0fb02d6fd30a37689e5961282cbdd873c90630f3
Imran Hossain <blur-imran@hotmail.com>
2023-07-24 07:29:57 +0600

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setProducts } from '../redux/products/productSlice';
-import demoData from '../demo/demo.json';
+import demoData from '../demo/demo.json'; 
 
 const HomePage = () => {
   const dispatch = useDispatch();
