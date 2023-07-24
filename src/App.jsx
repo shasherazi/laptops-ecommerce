8063f1a94c84ddda4d0648f74f0e12b9cf044b54
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-24 12:03:56 +0200

    Nav design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 import Home from "./pages/Home";
+import Header from './components/header/Header';
+import './style/home.css'
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => (
+    <header>
+      <a>Vespa</a>
+      <nav>
+        <ul>
+          <li><a href="#" className="single-nav">Home</a></li>
+          <li><a href="#" className="single-nav">Lifestyle</a></li>
+          <li><a href="#" className="single-nav">Shop</a></li>
+          <li><a href="#" className="single-nav">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+
+  export default Header;
