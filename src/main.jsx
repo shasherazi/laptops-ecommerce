6a3bbc454c92ef2e80e7c924c23ecc3d7e5e2831
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-25 15:29:05 +0200

    Use same nav for all

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
+      <Provider store={store}>
         <App />
-        <ToastContainer />
       </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import "../app.css";
-import Home from "../pages/Home";
-import Header from '../components/header/Header';
-import '../style/home.css'
-import menu from '../assets/menu.png'
+// import { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import "../app.css";
+// import Home from "../pages/Home";
+// import Header from '../components/header/Header';
+// import '../style/home.css'
+// import menu from '../assets/menu.png'
 
-function Main() {
-  const [isMenuActive, setIsMenuActive] = useState(false);
+// function Main() {
+//   const [isMenuActive, setIsMenuActive] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuActive(!isMenuActive);
-  };
-  return (
-    <main>
-      <Header isMenuActive={isMenuActive} setIsMenuActive={setIsMenuActive} />
-      <section className='main-content'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <img
-          src={menu}
-          className='menu'
-          onClick={toggleMenu}
-        />
-      </section>
-    </main>
-  );
-}
+//   const toggleMenu = () => {
+//     setIsMenuActive(!isMenuActive);
+//   };
+//   return (
+//     <main>
+//       <Header isMenuActive={isMenuActive} setIsMenuActive={setIsMenuActive} />
+//       <section className='main-content'>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//         </Routes>
+//         <img
+//           src={menu}
+//           className='menu'
+//           onClick={toggleMenu}
+//         />
+//       </section>
+//     </main>
+//   );
+// }
 
-export default Main;
+// export default Main;
