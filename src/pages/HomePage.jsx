6fcf29b7196892a99ac787dd73589140c9b6dcd1
Imran Hossain <blur-imran@hotmail.com>
2023-07-25 21:27:24 +0600

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,9 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setProducts } from '../redux/products/productSlice';
-import demoData from '../demo/demo.json'; 
+import ProductList from './productList';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const products = useSelector((state) => state.products.products);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('demo/demo.json');
-  //       const data = await response.json();
-  //       dispatch(setProducts(data)); 
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(setProducts(demoData));
-  }, [dispatch]);
-
   return (
-    <div className='pro-list'>
-      <h1>Product List</h1>
-      <ul className='products'>
-        {products.map((product) => (
-          <li key={product.id} className='product'>
-            <img src={product.picture} alt={product.name}/>
-            <h2>{product.name}</h2>
-            <button>View Details</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ProductList />
   );
-};
+}
 
 export default HomePage;
