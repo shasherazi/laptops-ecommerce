7e61ef65ab32d1c4da7237151ea8daceaaba3174
Imran Hossain <blur-imran@hotmail.com>
2023-07-25 21:27:32 +0600

    Update productSlice.js

--- src/redux/products/productSlice.js
+++ src/redux/products/productSlice.js
@@ -1,16 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import demoData from '../../demo/demo.json';
 
-const productSlice = createSlice({
+const productUrl = demoData;
+const initialState = {
+  products: [],
+  isLoading: false,
+  isError: false,
+};
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+  try {
+    // const response = await fetch(productUrl);
+    // const products = await response.json();
+    return productUrl;
+  } catch (err) {
+    return err;
+  }
+});
+
+export const productSlice = createSlice({
   name: 'products',
-  initialState: {
-    products: [], 
-  },
-  reducers: {
-    setProducts: (state, action) => {
-      state.products = action.payload; 
-    },
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => ({
+        ...state,
+        isLoading: true,
+        isError: false,
+      }))
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        const newStacks = [];
+        action.payload.map((product) => (
+          newStacks.push({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            picture: product.picture,
+          })
+        ));
+        return ({
+          ...state,
+          isLoading: false,
+          isError: false,
+          products: newStacks,
+        });
+      })
+      .addCase(fetchProducts.rejected, (state) => ({
+        ...state,
+        isLoading: false,
+        isError: true,
+      }));
   },
 });
 
-export const { setProducts } = productSlice.actions;
 export default productSlice.reducer;
