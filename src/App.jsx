c36ce4a9eb309b35164ba801f12ff5ba392dbeca
Imran Hossain <blur-imran@hotmail.com>
2023-07-26 14:55:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Splash from "./pages/Splash";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/Homepage" element={<HomePage />} />
+          <Route path="/Homepage/productdetail/:productId" element={<ProductDetailsPage />} />
+          <Route path="/" element={<Splash />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/home" element={<Home />} /> */}
+        </Routes>
     </BrowserRouter>
   );
 }
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/productdetail/:productId" element={<ProductDetailsPage />} />
-      </Routes>
-    </Router>
-  );
-};
-
 export default App;
