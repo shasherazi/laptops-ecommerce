42252d26a6b4f45a2b358519788f670df75868e6
Imran Hossain <blur-imran@hotmail.com>
2023-07-26 14:55:09 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
