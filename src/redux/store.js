886b3694b7e85751cf6a279c04f6523c74a6a0c2
Imran Hossain <blur-imran@hotmail.com>
2023-07-26 14:17:22 +0600

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './products/productSlice';
 import selectedProductReducer from './products/selectedProductSlice'; 
+import reservationReducer from './reservations/reservationSlice';
 
 const store = configureStore({
   reducer: {
     products: productReducer,
     productDetails: selectedProductReducer,
+    reservations: reservationReducer,
   },
 });
 
