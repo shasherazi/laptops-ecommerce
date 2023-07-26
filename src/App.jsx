bd48bfb8c30f4f63304d1c1cab56c7e1304670ff
Imran Hossain <blur-imran@hotmail.com>
2023-07-26 14:17:12 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
+import ReservationPage from "./pages/ReservationPage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/productdetail/:productId" element={<ProductDetailsPage />} />
+        <Route path="/my-reservation" element={<ReservationPage />} />
       </Routes>
     </Router>
   );
