fba0423f7975eee6c5542fc27d884f441bd39748
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-27 16:03:47 +0200

    Finish basic layout

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,9 +1,18 @@
 import '../css/reservation.css'
+import { Link } from 'react-router-dom';
 
 const Reservation = () => {
     return (
         <div className='reserve-section'>
-            <h1>Reservation</h1>
+            <h1>Book a vespa Testa-Ride</h1>
+            <hr/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, ad.</p>
+            <div className="button-container">
+                <Link  to="#!" className="">London</Link >
+                <Link  to="#!" className="">Reserve</Link >
+            </div>
+
+
         </div>
     )
 }
