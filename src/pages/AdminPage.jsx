91b26324877c31dcbad5cdd2aef734af456fe559
Imran Hossain <blur-imran@hotmail.com>
2023-07-28 11:09:39 +0600

    Update AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,32 +1,118 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts, addProduct, deleteProduct } from '../redux/products/productSlice';
+import './AdminPage.css';
 
 const AdminProductManagement = () => {
   const dispatch = useDispatch();
   const [productName, setProductName] = useState('');
   const [productPrice, setProductPrice] = useState('');
+  const [productPicture, setProductPicture] = useState('');
+  const [productCPU, setProductCPU] = useState('');
+  const [productMemory, setProductMemory] = useState('');
+  const [productStorage, setProductStorage] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
   const { products, isLoading, isError } = useSelector((state) => state.products);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-  const handleAddProduct = () => {
-    const newProduct = {
-      name: productName,
-      price: productPrice,
-    };
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    price: '',
+    picture: '',
+    cpu: '',
+    memory: '',
+    storage: '',
+  });
 
-    dispatch(addProduct(newProduct));
-    setProductName('');
-    setProductPrice('');
+  const validateForm = () => {
+    let isValid = true;
+    const errors = {};
+
+    if (!productName.trim()) {
+      isValid = false;
+      errors.name = 'Product name is required';
+    }
+    if (!productPicture.trim()) {
+      isValid = false;
+      errors.picture = 'Product picture is required';
+    }
+    if (!productCPU.trim()) {
+      isValid = false;
+      errors.cpu = 'Product cpu is required';
+    }
+    if (!productMemory.trim()) {
+      isValid = false;
+      errors.memory = 'Product memory is required';
+    }
+    if (!productStorage.trim()) {
+      isValid = false;
+      errors.storage = 'Product storage is required';
+    }
+
+
+    if (!productPrice.trim()) {
+      isValid = false;
+      errors.price = 'Product price is required';
+    } else if (isNaN(productPrice) || +productPrice <= 0) {
+      isValid = false;
+      errors.price = 'Product price must be a positive number';
+    }
+
+    // Add validation for other fields (picture, cpu, memory, storage) if needed
+
+    setFormErrors(errors);
+    return isValid;
+  };
+
+  const handleAddProduct = () => {
+    const isValid = validateForm();
+
+    if (isValid) {
+      // Create a new product object with the entered data
+      const newProduct = {
+        name: productName,
+        price: productPrice,
+        picture: productPicture,
+        cpu: productCPU,
+        memory: productMemory,
+        storage: productStorage,
+        // Add other product details here if needed
+      };
+
+      // Dispatch the addProduct action to add the new product
+      dispatch(addProduct(newProduct));
+
+      // Clear the input fields after adding the product
+      setProductName('');
+      setProductPrice('');
+      setProductPicture('');
+      setProductCPU('');
+      setProductMemory('');
+      setProductStorage('');
+      setFormErrors({
+        name: '',
+        price: '',
+        picture: '',
+        cpu: '',
+        memory: '',
+        storage: '',
+      });
+    }
   };
 
   const handleDeleteProduct = (productId) => {
+    // Dispatch the deleteProduct action to remove the product with the given productId
     dispatch(deleteProduct(productId));
   };
 
+  const handleSearch = () => {
+    // Perform search based on the searchKeyword
+    // For demonstration purposes, we'll just log the searchKeyword to the console
+    console.log('Searching for:', searchKeyword);
+  };
 
   if (isLoading) {
     return <h3>Loading...</h3>;
@@ -35,36 +121,92 @@ const AdminProductManagement = () => {
   }
 
   return (
-    <div>
+    <section className='admin-page'>
       <h1>Admin Product Management</h1>
-      <div>
-        <h2>Add Product</h2>
-        <input
-          type="text"
-          placeholder="Product Name"
-          value={productName}
-          onChange={(e) => setProductName(e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Product Price"
-          value={productPrice}
-          onChange={(e) => setProductPrice(e.target.value)}
-        />
-        <button onClick={handleAddProduct}>Add Product</button>
+      <div className='admin-page-content'>
+        <div className='admin-page-add'>
+          <h2>Add Product</h2>
+          <div className='from-fields'>
+            <label>Product Name:</label>
+            <input
+              type="text"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+            {formErrors.name && <span className="error">{formErrors.name}</span>}
+          </div>
+          <div className='from-fields'>
+            <label>Product Price:</label>
+            <input
+              type="text"
+              value={productPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+            {formErrors.price && <span className="error">{formErrors.price}</span>}
+          </div>
+          <div className='from-fields'>
+            <label>Product Picture URL:</label>
+            <input
+              type="text"
+              value={productPicture}
+              onChange={(e) => setProductPicture(e.target.value)}
+            />
+            {formErrors.picture && <span className="error">{formErrors.picture}</span>}
+          </div>
+          <div className='from-fields'>
+            <label>Product CPU:</label>
+            <input
+              type="text"
+              value={productCPU}
+              onChange={(e) => setProductCPU(e.target.value)}
+            />
+            {formErrors.cpu && <span className="error">{formErrors.cpu}</span>}
+          </div>
+          <div className='from-fields'>
+            <label>Product Memory:</label>
+            <input
+              type="text"
+              value={productMemory}
+              onChange={(e) => setProductMemory(e.target.value)}
+            />
+            {formErrors.memory && <span className="error">{formErrors.memory}</span>}
+          </div>
+          <div className='from-fields'>
+            <label>Product Storage:</label>
+            <input
+              type="text"
+              value={productStorage}
+              onChange={(e) => setProductStorage(e.target.value)}
+            />
+            {formErrors.storage && <span className="error">{formErrors.storage}</span>}
+          </div>
+          <button onClick={handleAddProduct}>Add Product</button>
+        </div>
+        <div className='admin-page-show'>
+          <div className='admin-page-search'>
+            <h2>Search Product</h2>
+            <input
+              type="text"
+              placeholder="Enter Keyword"
+              value={searchKeyword}
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+          <div className='admin-page-list'>
+            <h2>Product List</h2>
+            <ul>
+              {products.map((product) => (
+                <li key={product.id}>
+                  <p>{product.name}</p>
+                  <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div>
-        <h2>Product List</h2>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id} >
-              {product.name}
-              <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </section>
   );
 };
 
