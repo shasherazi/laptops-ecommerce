174e2f42ef848454c7490a09c4fbff9415a2e222
Imran Hossain <blur-imran@hotmail.com>
2023-07-28 19:19:03 +0600

    Update ProductDetailsPage.jsx

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -63,8 +63,8 @@ export default function ProductDetailsPage() {
             <h3>{product.name}</h3>
             <p>Price: ${product.price}</p>
             <p>Cpu: {product.cpu}</p>
-            <p>memory: {product.memory}</p>
-            <p>storage: {product.storage}</p>
+            <p>memory: {product.memory}GB</p>
+            <p>storage: {product.storage}GB</p>
           </div>
           <div className='pro-details-reserve'>
             <button type='button'>Reserve</button>
