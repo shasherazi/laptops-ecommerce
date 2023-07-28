7e4184cc533a8114f31c4e249971a9b81b0c209a
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-07-28 12:27:45 +0200

    Fix linters and add form of reservation

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import '../css/reservation.css'
-import { Link } from 'react-router-dom';
-import search from '../assets/search.png'
+import cities from '../demo/city.json';
 
 const Reservation = () => {
+  const [selectedCity, setSelectedCity] = useState('');
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+  const filteredCities = cities.filter(cit => cit.city !== null);
+  filteredCities.sort((a, b) => a.city.localeCompare(b.city));
+
     return (
         <div className='reserve-section'>
             <h1>Book a vespa Testa-Ride</h1>
             <hr/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, ad.</p>
             <div className="button-container">
-                <Link  to="#!" className="">London</Link >
-                <Link  to="#!" className="">Reserve</Link >
+                <form>
+                <select value={selectedCity} onChange={handleCityChange}>
+                    {filteredCities.map(cit => (
+                    <option key={cit.city} value={cit.city}>{cit.city}</option>
+                    ))}
+                </select>
+                <input type='number' placeholder='Enter quantity' min="1"  />
+                <button type='submit'>Reserve</button>
+                </form>
             </div>
-            <img className='search' src={search} />
         </div>
     )
 }
