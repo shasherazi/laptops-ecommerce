28e0a2ad1e4f52bc2c42cf072e906977a59d468f
Imran Hossain <blur-imran@hotmail.com>
2023-08-01 12:46:29 +0600

    Update ReservationPage.jsx

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -2,13 +2,11 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchReservations, removeReservation } from '../redux/reservations/reservationSlice';
-// import { fetchProducts } from '../redux/products/productSlice';
-// import laptop_reserveSlice, { fetchLaptopReservations } from '../redux/reservations/laptop_reserveSlice';
 import './reservationPage.css';
 
 export default function ReservationPage() {
   const dispatch = useDispatch();
-  const { reservations, isLoading, isError } = useSelector((store) => store.reservations);
+  const { reservations, isLoading, isError } = useSelector((state) => state.reservations);
 
   useEffect(() => {
     dispatch(fetchReservations());
@@ -20,7 +18,8 @@ export default function ReservationPage() {
 
   if (isLoading) {
     return <h3>Loading...</h3>;
-  } if (isError) {
+  }
+  if (isError) {
     return <h3>Something went wrong...</h3>;
   }
 
@@ -29,14 +28,16 @@ export default function ReservationPage() {
       <h1>Reservation List</h1>
       <div className='reservation-new'>
         <Link to="/">
-              <button type='button'>Add Reservation</button>
+          <button type='button'>Add Reservation</button>
         </Link>
       </div>
       {reservations.map((reservation) => (
         <div key={reservation.id} className='reservation-list'>
           <div className='reservation-list-info'>
             <p>ID: {reservation.id}</p>
-            <button type='button' onClick={() => handleRemoveReservation(reservation.id)}>Remove</button>
+            <button type='button' onClick={() => handleRemoveReservation(reservation.id)}>
+              Remove
+            </button>
           </div>
         </div>
       ))}
