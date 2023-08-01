bfacd768acafd7bb312d4509bb48e4b25e08d5ce
fuadmoin <fuadamoin@gmail.com>
2023-08-01 15:41:50 +0300

    Fix the test for reservation page.

--- src/__test__/reservationpage.test.js
+++ src/__test__/reservationpage.test.js
@@ -1,69 +1,66 @@
-/*eslint-disable*/
 import React from 'react';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { render /*,fireEvent */} from '@testing-library/react';
 import configureStore from 'redux-mock-store';
 import ReservationPage from '../pages/ReservationPage';
-// import { fetchReservations, removeReservation } from '../redux/reservations/reservationSlice';
 import { MemoryRouter } from 'react-router-dom';
-
 
 const mockStore = configureStore([]);
 
 describe('ReservationPage', () => {
   let store;
-  let component;
 
   beforeEach(() => {
     store = mockStore({
       reservations: {
         reservations: [
           {
-            id: 1,
-            productName: 'Product 1',
-            city: 'City 1',
-            quantity: 2,
+            reservation: {
+              id: 1,
+            },
+            laptop: {
+              name: 'Laptop 1',
+            },
+            laptop_reservation: {
+              city: 'City 1',
+              quantity: 2,
+            },
           },
           {
-            id: 2,
-            productName: 'Product 2',
-            city: 'City 2',
-            quantity: 3,
+            reservation: {
+              id: 2,
+            },
+            laptop: {
+              name: 'Laptop 2',
+            },
+            laptop_reservation: {
+              city: 'City 2',
+              quantity: 3,
+            },
           },
         ],
         isLoading: false,
         isError: false,
       },
     });
+  });
 
-    store.dispatch = jest.fn();
-
-    component = render(
-      
+  test('renders reservation list', () => {
+    render(
       <Provider store={store}>
-          <MemoryRouter>
-        <ReservationPage />
+        <MemoryRouter>
+          <ReservationPage />
         </MemoryRouter>
-      </Provider>,
+      </Provider>
     );
-  });
 
-  it('should dispatch fetchReservations action on mount', () => {
-    expect(store.dispatch).toHaveBeenCalledWith(fetchReservations());
-  });
-
-  it('should display reservation list', () => {
-    expect(component.getByText('Product 1')).toBeInTheDocument();
-    expect(component.getByText('City: City 1')).toBeInTheDocument();
-    expect(component.getByText('Quantity: 2')).toBeInTheDocument();
-    expect(component.getByText('Product 2')).toBeInTheDocument();
-    expect(component.getByText('City: City 2')).toBeInTheDocument();
-    expect(component.getByText('Quantity: 3')).toBeInTheDocument();
-  });
-
-  it('should dispatch removeReservation action on remove button click', () => {
-    fireEvent.click(component.getAllByText('Remove')[0]);
-    expect(store.dispatch).toHaveBeenCalledWith(removeReservation(1));
+    expect(screen.getByText('Reservation List')).toBeInTheDocument();
+    expect(screen.getByText('Laptop 1')).toBeInTheDocument();
+    expect(screen.getByText('City: City 1')).toBeInTheDocument();
+    expect(screen.getByText('Quantity: 2')).toBeInTheDocument();
+    expect(screen.getByText('Laptop 2')).toBeInTheDocument();
+    expect(screen.getByText('City: City 2')).toBeInTheDocument();
+    expect(screen.getByText('Quantity: 3')).toBeInTheDocument();
   });
 });
