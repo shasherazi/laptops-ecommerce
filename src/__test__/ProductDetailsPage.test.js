769aa9398fcff82a84b49d686b12cad7526b6898
Imran Hossain <blur-imran@hotmail.com>
2023-08-01 22:50:20 +0600

    Update ProductDetailsPage.test.js

--- src/__test__/ProductDetailsPage.test.js
+++ src/__test__/ProductDetailsPage.test.js
@@ -1,15 +1,14 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// ProductDetailsPage.test.js
-import { React } from 'react';
+/* eslint-disable no-undef */
+import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-// import { Provider } from 'react-redux';
-// import { MemoryRouter } from 'react-router-dom'; // Use MemoryRouter for testing
-// import store from '../redux/store'; // Import your actual Redux store
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import ProductDetailsPage from '../pages/ProductDetailsPage';
+import mockStore from './mockStore';
 
 describe('ProductDetailsPage', () => {
-  // Sample product data for testing
   const sampleProduct = {
     id: 1,
     name: 'Sample Laptop',
@@ -21,10 +20,18 @@ describe('ProductDetailsPage', () => {
   };
 
   test('renders product details correctly when user is logged in', () => {
-    // Render the component with actual Redux store and React context
-    render(<ProductDetailsPage />);
+    const storeWithLogin = mockStore({
+      productDetails: { productDetails: [sampleProduct] },
+      user: { isLogin: true },
+    });
+    render(
+      <Provider store={storeWithLogin}>
+        <MemoryRouter>
+          <ProductDetailsPage />
+        </MemoryRouter>
+      </Provider>
+    );
 
-    // Verify that the product details are displayed correctly
     expect(screen.getByRole('product')).toBeInTheDocument();
     expect(screen.getByText(sampleProduct.name)).toBeInTheDocument();
     expect(screen.getByText(`Price: $${sampleProduct.price}`)).toBeInTheDocument();
@@ -32,27 +39,23 @@ describe('ProductDetailsPage', () => {
     expect(screen.getByText(`memory: ${sampleProduct.memory} GB RAM`)).toBeInTheDocument();
     expect(screen.getByText(`storage: ${sampleProduct.storage} GB SSD`)).toBeInTheDocument();
 
-    // Verify that the "Reserve" button is present since the user is logged in
     expect(screen.getByText('Reserve')).toBeInTheDocument();
   });
 
-  // test('renders "Please login to reserve" message when user is not logged in', () => {
-  //   // Modify the Redux store state to simulate the user not being logged in
-  //   const storeWithoutLogin = { ...store, getState: () => ({ productDetails: { productDetails: [sampleProduct] }, user: { isLogin: false } }) };
+  test('renders "Please login to reserve" message when user is not logged in', () => {
+    const storeWithoutLogin = mockStore({
+      productDetails: { productDetails: [sampleProduct] },
+      user: { isLogin: false },
+    });
+    render(
+      <Provider store={storeWithoutLogin}>
+        <MemoryRouter>
+          <ProductDetailsPage />
+        </MemoryRouter>
+      </Provider>
+    );
 
-  //   // Render the component with the modified Redux store and React context
-  //   render(
-  //     <Provider store={storeWithoutLogin}>
-  //       <MemoryRouter>
-  //         <ProductDetailsPage />
-  //       </MemoryRouter>
-  //     </Provider>
-  //   );
-
-  //   // Verify that the "Please login to reserve" message is present since the user is not logged in
-  //   expect(screen.getByText('Please login to reserve')).toBeInTheDocument();
-
-  //   // Verify that the "Reserve" button is not present
-  //   expect(screen.queryByText('Reserve')).not.toBeInTheDocument();
-  // });
+    expect(screen.getByText('Please login to reserve')).toBeInTheDocument();
+    expect(screen.queryByText('Reserve')).not.toBeInTheDocument();
+  });
 });
